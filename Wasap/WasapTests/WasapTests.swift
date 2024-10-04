611d600646ce347ed0acad6054e4b2f6716f28fa
chongin12(Mosu) <chongin12@naver.com>
2024-10-04 21:34:25 +0900

    이슈 템플릿 이름 수정 & 테스트 타겟 추가

---
+++ Wasap/WasapTests/WasapTests.swift
@@ -0,0 +1,16 @@
+//
+//  WasapTests.swift
+//  WasapTests
+//
+//  Created by chongin on 10/4/24.
+//
+
+import Testing
+
+struct WasapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
