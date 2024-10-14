56d5f037de51c6e7ebea76f15b4a17b41a3f8b92
chongin12(Mosu) <chongin12@naver.com>
2024-10-14 23:05:06 +0900

    [#17] 커스텀 슬라이더 구현

--- Wasap/Wasap/Features/WifiAutoConnect/View/CameraView.swift
+++ Wasap/Wasap/Features/WifiAutoConnect/View/CameraView.swift
@@ -40,6 +40,21 @@ final class CameraView: BaseView {
         return button
     }()
 
+    public lazy var zoomControlButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(systemName: "plus.magnifyingglass"), for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = .clear
+        button.addTarget(self, action: #selector(showZoomSliderView), for: .touchUpInside)
+        return button
+    }()
+
+    public lazy var zoomSliderView: UISlider = {
+        let slider = CustomSlider()
+
+        return slider
+    }()
+
     private lazy var opaqueBackgroundView: UIView = {
         let view = UIView()
         view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
@@ -70,11 +85,12 @@ final class CameraView: BaseView {
 
     override func layoutSubviews() {
         super.layoutSubviews()
+
         previewLayer?.frame = previewContainerView.bounds
     }
 
     func setViewHierarchy() {
-        self.addSubViews(previewContainerView, opaqueBackgroundView, capturedImageView, takePhotoButton)
+        self.addSubViews(previewContainerView, opaqueBackgroundView, capturedImageView, takePhotoButton, zoomControlButton, zoomSliderView)
     }
 
     func setConstraints() {
@@ -96,5 +112,157 @@ final class CameraView: BaseView {
             $0.width.height.equalTo(85)
         }
 
+        zoomControlButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(takePhotoButton.snp.top).offset(-16)
+            $0.width.height.equalTo(50)
+        }
+
+        zoomSliderView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(zoomControlButton.snp.top).offset(-16)
+            $0.width.equalToSuperview().multipliedBy(0.8)
+            $0.height.equalTo(50)
+        }
+
+    }
+
+    @objc private func showZoomSliderView() {
+        zoomSliderView.isHidden.toggle()
+    }
+}
+
+// https://stackoverflow.com/questions/69817749/add-ticker-to-snapping-uislider
+protocol CustomSliderDelegate: NSObject {
+    func sliderChanged(_ newValue: Int, sender: Any)
+}
+extension CustomSliderDelegate {
+    // make this delegate func optional
+    func sliderChanged(_ newValue: Int, sender: Any) {}
+}
+
+class CustomSlider: UISlider {
+    var delegate: CustomSliderDelegate?
+    var stepCount: Int
+    init(stepCount: Int = 50) {
+        self.stepCount = stepCount
+        super.init(frame: .zero)
+        let thunbImage = UIGraphicsImageRenderer(size: CGSize(width: 4, height: 70)).image { context in
+            UIColor.white.setFill()
+            context.fill(CGRect(x: 0, y: 0, width: 4, height: 70))
+        }
+        self.setThumbImage(thunbImage, for: .normal)
+        self.minimumValue = 1.0
+        self.maximumValue = 10.0
+        self.value = 1.0
+        self.tintColor = .white
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func trackRect(forBounds bounds: CGRect) -> CGRect {
+        var result = super.trackRect(forBounds: bounds)
+        result.size.height = 0
+        return result
+    }
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+
+        // get the track rect
+        let trackR: CGRect = super.trackRect(forBounds: bounds)
+
+        // get the thumb rect at min and max values
+        let minThumbR: CGRect = self.thumbRect(forBounds: bounds, trackRect: trackR, value: minimumValue)
+        let maxThumbR: CGRect = self.thumbRect(forBounds: bounds, trackRect: trackR, value: maximumValue)
+
+        // usable width is center of thumb to center of thumb at min and max values
+        let usableWidth: CGFloat = maxThumbR.midX - minThumbR.midX
+
+        // Tick Height (or use desired explicit height)
+        let tickHeight: CGFloat = bounds.height
+
+        // "gap" between tick marks
+        let stepWidth: CGFloat = usableWidth / CGFloat(stepCount)
+
+        // a reusable path
+        var pth: UIBezierPath!
+
+        // a reusable point
+        var pt: CGPoint!
+
+        // new path
+        pth = UIBezierPath()
+
+        // left end of our track rect
+        pt = CGPoint(x: minThumbR.midX, y: bounds.height * 0.5)
+
+        // top of vertical tick lines
+        pt.y = (bounds.height - tickHeight) * 0.5
+
+        // we have to draw stepCount + 1 lines
+        //  so use
+        //      0...stepCount
+        //  not
+        //      0..<stepCount
+
+        let valueToStep = Int((Double(value - minimumValue) / Double(maximumValue - minimumValue)) * Double(stepCount))
+        for i in 0...valueToStep {
+            if i%5 != 0 {
+                pt.y += tickHeight * 0.25
+                pth.move(to: pt)
+                pth.addLine(to: CGPoint(x: pt.x, y: pt.y + tickHeight * 0.5))
+                pt.y -= tickHeight * 0.25
+            } else {
+                pth.move(to: pt)
+                pth.addLine(to: CGPoint(x: pt.x, y: pt.y + tickHeight * 0.75))
+            }
+            pt.x += stepWidth
+        }
+        UIColor.white.setStroke()
+        pth.stroke()
+
+        pth = UIBezierPath()
+
+        for i in valueToStep+1...stepCount {
+            if i%5 != 0 {
+                pt.y += tickHeight * 0.25
+                pth.move(to: pt)
+                pth.addLine(to: CGPoint(x: pt.x, y: pt.y + tickHeight * 0.5))
+                pt.y -= tickHeight * 0.25
+            } else {
+                pth.move(to: pt)
+                pth.addLine(to: CGPoint(x: pt.x, y: pt.y + tickHeight * 0.75))
+            }
+            pt.x += stepWidth
+        }
+        UIColor.lightGray.setStroke()
+        pth.stroke()
+    }
+
+    override func setValue(_ value: Float, animated: Bool) {
+        // don't allow value outside range of min and max values
+        let newVal: Float = min(max(minimumValue, value), maximumValue)
+        super.setValue(newVal, animated: animated)
+
+        // we need to trigger draw() when the value changes
+        setNeedsDisplay()
+        let steps: Float = Float(stepCount)
+        let rng: Float = maximumValue - minimumValue
+        // get the percentage along the track
+        let pct: Float = newVal / rng
+        // use that pct to get the rounded step position
+        let pos: Float = round(steps * pct)
+        // tell the delegate which Tick the thumb snapped to
+        delegate?.sliderChanged(Int(pos), sender: self)
+    }
+
+    override var bounds: CGRect {
+        willSet {
+            // we need to trigger draw() when the bounds changes
+            setNeedsDisplay()
+        }
     }
 }
