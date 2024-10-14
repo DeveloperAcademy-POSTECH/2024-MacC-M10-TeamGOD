befa7e40d9b58c8b8e956823002472f3a925d01d
chongin12(Mosu) <chongin12@naver.com>
2024-10-14 20:59:14 +0900

    [#17] 카메라 마스킹, 셔터 버튼 뷰 추가

--- Wasap/Wasap/Features/WifiAutoConnect/View/CameraView.swift
+++ Wasap/Wasap/Features/WifiAutoConnect/View/CameraView.swift
@@ -11,7 +11,16 @@ import AVFoundation
 
 final class CameraView: BaseView {
     var previewLayer: AVCaptureVideoPreviewLayer?
-    lazy var previewContainerView: UIView = {
+    private var superViewWidth: CGFloat {
+        self.frame.size.width
+    }
+    private var superViewHeight: CGFloat {
+        self.frame.size.height
+    }
+    private var maskRect: CGRect {
+        CGRect(origin: CGPoint(x: superViewWidth * 0.1, y: superViewHeight * 0.35), size: CGSize(width: superViewWidth * 0.8, height: superViewHeight * 0.3))
+    }
+    public lazy var previewContainerView: UIView = {
         UIView()
     }()
     private lazy var capturedImageView: UIImageView = {
@@ -21,12 +30,42 @@ final class CameraView: BaseView {
         return imageView
     }()
     private lazy var takePhotoButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Take Photo", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = .systemBlue
-        button.layer.cornerRadius = 8
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(systemName: "inset.filled.circle"), for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = .clear
+        button.contentHorizontalAlignment = .fill
+        button.contentVerticalAlignment = .fill
+        button.contentMode = .scaleAspectFit
         return button
+    }()
+
+    private lazy var opaqueBackgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+
+        let maskLayer = CAShapeLayer()
+        let path = CGMutablePath()
+
+        path.addRects([
+            CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: superViewWidth, height: superViewHeight)),
+            maskRect
+        ])
+        maskLayer.path = path
+        maskLayer.fillRule = .evenOdd
+
+        view.layer.mask = maskLayer
+
+        let borderLayer = CAShapeLayer()
+        let borderPath = UIBezierPath(rect: maskRect)
+        borderLayer.path = borderPath.cgPath
+        borderLayer.strokeColor = UIColor.cameraFrameGreen.cgColor
+        borderLayer.lineWidth = 6.0
+        borderLayer.fillColor = UIColor.clear.cgColor
+
+        view.layer.addSublayer(borderLayer)
+
+        return view
     }()
 
     override func layoutSubviews() {
@@ -35,11 +74,15 @@ final class CameraView: BaseView {
     }
 
     func setViewHierarchy() {
-        self.addSubViews(previewContainerView, capturedImageView, takePhotoButton)
+        self.addSubViews(previewContainerView, opaqueBackgroundView, capturedImageView, takePhotoButton)
     }
 
     func setConstraints() {
         previewContainerView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+        opaqueBackgroundView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
 
@@ -50,6 +93,8 @@ final class CameraView: BaseView {
         takePhotoButton.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.bottom.equalTo(safeAreaLayoutGuide).offset(-16)
+            $0.width.height.equalTo(85)
         }
+
     }
 }
