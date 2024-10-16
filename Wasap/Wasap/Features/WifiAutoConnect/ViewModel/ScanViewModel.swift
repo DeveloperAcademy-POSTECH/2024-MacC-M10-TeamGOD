ddf6c4f63dba24183ed1f5502eb608d47dfb78b7
Jonghyeon Chang <hokhmah91@naver.com>
2024-10-17 04:53:48 +0900

    [#28] boundingBox 코드 이동 및 로직 수정

--- Wasap/Wasap/Features/WifiAutoConnect/ViewModel/ScanViewModel.swift
+++ Wasap/Wasap/Features/WifiAutoConnect/ViewModel/ScanViewModel.swift
@@ -22,12 +22,12 @@ public class ScanViewModel: BaseViewModel {
     public var updatedImage: Driver<UIImage>
 
     // MARK: - Properties
-    private var ocrResult: Observable<([(CGRect, CGColor)], String, String)>
+    private var ocrResult: Observable<(UIImage, String, String)>
 
     public init(imageAnalysisUseCase: ImageAnalysisUseCase, coordinatorController: ScanCoordinatorController, previewImage: UIImage) {
         self.imageAnalysisUseCase = imageAnalysisUseCase
 
-        let ocrResultRelay = BehaviorRelay<([(CGRect, CGColor)], String, String)>(value: ([], "", ""))
+        let ocrResultRelay = BehaviorRelay<(UIImage, String, String)>(value: (previewImage, "", ""))
         self.ocrResult = ocrResultRelay.asObservable().share()
 
         let updatedImageRelay = BehaviorRelay<UIImage?>(value: nil)
@@ -39,7 +39,7 @@ public class ScanViewModel: BaseViewModel {
         viewDidLoad
             .observe(on: MainScheduler.asyncInstance)
             .withUnretained(self)
-            .flatMapLatest { owner, _ in
+            .flatMapLatest { owner, _ -> Single<(UIImage, String, String)> in
                 imageAnalysisUseCase.performOCR(on: previewImage)
             }
             .subscribe {
@@ -51,8 +51,8 @@ public class ScanViewModel: BaseViewModel {
             .disposed(by: disposeBag)
 
         ocrResult
-            .subscribe { boundingBoxes, _, _ in
-                updatedImageRelay.accept(self.drawBoundingBoxes(boundingBoxes, on: previewImage))
+            .subscribe { imageWithBoxes, _, _ in
+                updatedImageRelay.accept(imageWithBoxes)
             }
             .disposed(by: disposeBag)
 
@@ -65,29 +65,5 @@ public class ScanViewModel: BaseViewModel {
             .disposed(by: disposeBag)
     }
     
-    // bounding box를 이미지 위에 그리는 함수
-    private func drawBoundingBoxes(_ boxes: [(CGRect, CGColor)], on image: UIImage) -> UIImage? {
-        let imageSize = image.size
-        UIGraphicsBeginImageContextWithOptions(imageSize, false, 0)
-        let context = UIGraphicsGetCurrentContext()
-        
-        image.draw(at: .zero)
-        
-        for (box, color) in boxes {
-            let rect = CGRect(
-                x: box.origin.x * imageSize.width,
-                y: (1 - box.origin.y - box.height) * imageSize.height,
-                width: box.width * imageSize.width,
-                height: box.height * imageSize.height
-            )
-            context?.setStrokeColor(color)
-            context?.setLineWidth(2.0)
-            context?.stroke(rect)
-        }
-        
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return newImage
-    }
+    
 }
